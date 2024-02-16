51a0a6211365aa0ca5d5fb4dfb96aa31459eacb8
Abhishekgaded <gadedabhishek01@gmail.com>
2024-02-16 18:59:40 +0530

    Update Fruits.cs

--- Assets/Scripts/Fruits.cs
+++ Assets/Scripts/Fruits.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fruits : MonoBehaviour
+public class Fruits : MonoBehaviour //Inheritance
 {
 
     public GameObject whole;
